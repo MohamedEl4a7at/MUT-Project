d637e9d138c9e63883bde78026bab1b0ae7c4ca2
MohamedEl4a7at <mohamedmustafa93@std.mans.edu.eg>
2023-05-22 00:06:33 +0300

    Update

--- src/routers/articles.js
+++ src/routers/articles.js
@@ -7,7 +7,7 @@ const multer = require('multer')
 /////////////////////////upload with image
 const storage = multer.diskStorage({
     destination:  (req, file, cb)=> {
-      cb(null, 'uploads/')
+      cb(null, './uploads/')
     },
     filename:  (req, file, cb)=> {
       cb(null, Date.now() + '-' + file.originalname)
